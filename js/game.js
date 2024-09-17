b6fc5cda2af18094e68a83116771ab2319a700f8
Ivan Symkanych <ivan.symkanych24@gmail.com>
2024-09-17 16:55:11 +0300

    Add model loader

--- js/game.js
+++ js/game.js
@@ -1,14 +1,19 @@
 import * as THREE from 'three';
+import { GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+
 
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-let renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+const loader = new GLTFLoader();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 let geometry = new THREE.BoxGeometry(1, 1, 1);
 let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 let player = new THREE.Mesh(geometry, material);
+player.position.y = 1;
 scene.add(player);
 
 let trackGeometry = new THREE.PlaneGeometry(10, 1000);
@@ -23,7 +28,7 @@ camera.lookAt(player.position);
 
 let playerSpeed = 0.1;
 let playerLane = 0;
-const lanes = [-2, 0, 2];
+const lanes = [-3, 0, 3];
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowLeft') {
@@ -45,4 +50,3 @@ function animate() {
 }
 
 animate();
-

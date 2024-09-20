0c3baf4fbc65fb42ff2219bd3e44faf47bc2b466
Ivan Symkanych <ivan.symkanych24@gmail.com>
2024-09-20 12:52:43 +0300

    Update

--- js/game.js
+++ js/game.js
@@ -3,14 +3,22 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const loader = new GLTFLoader();
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-const lanes = [-3, 0, 3];
-const blueColor = new THREE.Color(0x0dabf0);
-const purpleColor = new THREE.Color(0x8d1bac);
-const orangeColor = new THREE.Color(0xe6a118);
 
+const camera = new THREE.PerspectiveCamera(
+    80,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  
+const GameColors = {
+    orange: 0xff8c00,
+    blue: 0x1685f8,
+    violet: 0x9d46dd,
+  };
+
+const lanes = [-3, 0, 3];
 let playerSpeed = 0.1;
 let playerLane = 0;
 let player;
@@ -24,16 +32,16 @@ function init(){
 };
 
 function createScene(){
+    setupLights();
+
     scene.background = new THREE.Color(0xcccccc);
     document.body.appendChild(renderer.domElement);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    directionalLight.position.set(5, 10, 7.5);
-    scene.add(directionalLight);
-
     loader.load('models/Stickman_with_anims.glb', (gltf) => {
         player = gltf.scene;
         scene.add(player);
+        console.log(player);
 
         playerMixer = new THREE.AnimationMixer(player);
 
@@ -43,33 +51,25 @@ function createScene(){
         console.log('Available animations:', animations);
       });
       
-    camera.position.set(0, 5, 10);
-    camera.lookAt(scene.position);
-
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'ArrowLeft') {
-            playerLane = Math.max(playerLane - 1, -1);
-        }
-        if (event.key === 'ArrowRight') {
-            playerLane = Math.min(playerLane + 1, 1);
-        }
-    });
 
     loader.load('/models/Track_parts.glb', (gltf) => {
         trackFlat = gltf.scene.children[1]; 
         for (let i = 0; i < 15; i++) {
-            spawnTrack(0, 0, -i * 25);
+            spawnTrack(0, 0, i * 25);
         }
     });
 
     loader.load('/models/Brain_model.glb', (gltf) => {
       brain = gltf.scene.children[0];
 
-      spawnBrain(1, 1, 0, blueColor);
-      spawnBrain(1, 2, 0, purpleColor);
-      spawnBrain(1, 3, 0, orangeColor);
+      spawnBrain(1, 1, 0, GameColors.blue);
+      spawnBrain(1, 2, 0, GameColors.orange);
+      spawnBrain(1, 3, 0, GameColors.v);
 
     });
+
+    camera.position.set(0, 8, -10);
+    camera.lookAt(scene.position);
 }
 
 function spawnBrain(x,y,z,color)
@@ -93,16 +93,28 @@ function spawnTrack(x, y, z) {
     scene.add(clone);        
 }
 
+function setupLights() {
+    let light = new THREE.DirectionalLight(0xb7b7b7, 0.9);
+    light.position.set(-3, 10, -12); //default; light shining from top
+    light.target.position.set(0, 0, 0);
+
+    scene.add(light); 
+    scene.add(light.target);
+
+    const ambLight = new THREE.AmbientLight(0xbcc7d6)
+    scene.add(ambLight);
+  };
+
 function animate() {
     requestAnimationFrame(animate);
 
     if (playerMixer) {
         playerMixer.update(0.01);
     }
-
-    // player.position.z -= playerSpeed;
-    // player.position.x = THREE.MathUtils.lerp(player.position.x, lanes[playerLane + 1], 0.1);
-    // camera.position.z = player.position.z + 10;
+    
+   // player.position.z += playerSpeed;
+   // player.position.x = THREE.MathUtils.lerp(player.position.x, lanes[playerLane + 1], 0.1);
+  //  camera.position.z = player.position.z + 10;
 
     renderer.render(scene, camera);
 };

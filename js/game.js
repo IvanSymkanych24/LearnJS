d30b12272e4140c7fbaa499594c90390a3e0d122
Ivan Symkanych <ivan.symkanych24@gmail.com>
2024-09-24 16:19:03 +0300

    Update

--- js/game.js
+++ js/game.js
@@ -10,26 +10,31 @@ const camera = new THREE.PerspectiveCamera(
     window.innerWidth / window.innerHeight,
     0.1,
     1000
-  );
-  
+);
+
 const GameColors = {
     orange: 0xff8c00,
     blue: 0x1685f8,
     violet: 0x9d46dd,
-  };
+};
 
-const lanes = [-3, 0, 3];
-let playerSpeed = 0.1;
-let playerLane = 0;
-let player;
+let playerModel;
 let playerMixer;
 let trackFlat;
 let brain;
 
+let playerPositionIndex = 1; 
+const lanePositions = [-3, 0, 3]; 
+let speed = 0.1;
+
+let isPlayerRun = false; 
+let isGameStart = false;
+
 function init(){
     createScene();
     animate();
-};
+    subciribeEvents();
+}
 
 function createScene(){
     setupLights();
@@ -39,18 +44,21 @@ function createScene(){
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     loader.load('models/Stickman_with_anims.glb', (gltf) => {
-        player = gltf.scene;
-        scene.add(player);
-        console.log(player);
-
-        playerMixer = new THREE.AnimationMixer(player);
-
+        playerModel = gltf.scene;
+        playerModel.position.x = lanePositions[playerPositionIndex];
+        scene.add(playerModel);
+        console.log(playerModel);
+        playerMixer = new THREE.AnimationMixer(playerModel);
         const animations = gltf.animations;
         const action = playerMixer.clipAction(animations[4]);
         action.play();
-        console.log('Available animations:', animations);
-      });
-      
+        
+        playerModel.traverse((child) => {
+          if (child.name === 'Plane') {
+          child.visible = false;
+        }});
+    });
+
 
     loader.load('/models/Track_parts.glb', (gltf) => {
         trackFlat = gltf.scene.children[1]; 
@@ -64,59 +72,95 @@ function createScene(){
 
       spawnBrain(1, 1, 0, GameColors.blue);
       spawnBrain(1, 2, 0, GameColors.orange);
-      spawnBrain(1, 3, 0, GameColors.v);
-
+      spawnBrain(1, 3, 0, GameColors.violet);
     });
 
     camera.position.set(0, 8, -10);
     camera.lookAt(scene.position);
 }
 
-function spawnBrain(x,y,z,color)
+function subciribeEvents()
 {
+    document.addEventListener('click', handleGameStart);
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowLeft') {
+            movePlayerLeft();
+        } else if (event.key === 'ArrowRight') {
+            movePlayerRight();
+        }
+    });
+}
+
+function handleGameStart(){
+    isPlayerRun = true;
+    isGameStart = true;
+}
+
+function movePlayerLeft() {
+  if (playerPositionIndex < 2) {
+      playerPositionIndex++;
+      playerModel.position.x = lanePositions[playerPositionIndex];
+  }
+}
+
+function movePlayerRight() {
+  if (playerPositionIndex > 0) {
+      playerPositionIndex--;
+      playerModel.position.x = lanePositions[playerPositionIndex];
+  }
+}
+
+function spawnBrain(x, y, z, color) {
     const clone = brain.clone();
-    clone.position.set(x,y,z);
+    clone.position.set(x, y, z);
 
     clone.traverse((child) => {
         if (child.isMesh) {
-          child.material = child.material.clone();
-          child.material.color.set(color);
+            child.material = child.material.clone();
+            child.material.color.set(color);
         }
-      });
+    });
 
     scene.add(clone);
 }
 
 function spawnTrack(x, y, z) {
-    const clone = trackFlat.clone();  
-    clone.position.set(x, y, z);    
-    scene.add(clone);        
+    const clone = trackFlat.clone();
+    clone.position.set(x, y, z);
+    scene.add(clone);
 }
 
 function setupLights() {
     let light = new THREE.DirectionalLight(0xb7b7b7, 0.9);
-    light.position.set(-3, 10, -12); //default; light shining from top
+    light.position.set(-3, 10, -12);
     light.target.position.set(0, 0, 0);
 
-    scene.add(light); 
+    scene.add(light);
     scene.add(light.target);
 
-    const ambLight = new THREE.AmbientLight(0xbcc7d6)
+    const ambLight = new THREE.AmbientLight(0xbcc7d6);
     scene.add(ambLight);
-  };
+}
 
 function animate() {
     requestAnimationFrame(animate);
 
-    if (playerMixer) {
-        playerMixer.update(0.01);
-    }
-    
-   // player.position.z += playerSpeed;
-   // player.position.x = THREE.MathUtils.lerp(player.position.x, lanes[playerLane + 1], 0.1);
-  //  camera.position.z = player.position.z + 10;
+    if (playerModel) {
+      if (isPlayerRun) {
+          playerModel.position.z += speed;
+      }
+
+      camera.position.z = playerModel.position.z - 10; 
+      camera.position.x = playerModel.position.x; 
+      camera.lookAt(playerModel.position);
+
+      if (playerMixer) {
+          playerMixer.update(0.01);
+      }
+  }
 
     renderer.render(scene, camera);
-};
+}
 
 init();
